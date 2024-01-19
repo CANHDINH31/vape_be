313c2e2610dc0f5ed821cae82aa0274f9f06c773
Nguyen Cuong <cuongtc981@gmail.com>
2024-01-19 07:08:48 +0700

    feat: update api

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,11 +1,51 @@
 const orderModel = require("../models/order.model");
+const nodemailer = require("nodemailer");
 
 module.exports = {
   create: async (req, res) => {
     try {
-      const data = await orderModel.create(req.body);
+      const data = req.body;
+      const payload = {
+        name: data?.name,
+        phone: data?.phone,
+        address: data?.address,
+        note: data?.note,
+        user: data?.user?._id,
+        cart: data?.cart?.map((e) => ({
+          product: e?.info?._id,
+          amount: e?.amount,
+          type: e?.type,
+        })),
+      };
+      const order = await orderModel.create(payload);
+
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: "dinhphamcanh@gmail.com",
+          pass: "zimxlcsohmqvqmto",
+        },
+      });
+
+      await transporter.sendMail({
+        from: "dinhphamcanh@gmail.com",
+        to: "dinhphamcanh@gmail.com",
+        subject: `JINVAPE - ĐƠN HÀNG MỚI`,
+        html: `<h1>Đơn hàng JINVAPE</h1>
+        <p>Mã đơn hàng: ${order?._id}</p>
+        <p>Khách hàng: ${data?.name}</p>
+        <p>Số điện thoại: ${data?.phone}</p>
+        <p>Địa chỉ: ${data?.address}</p>
+        <p>Ghi chú: ${data?.note}</p>
+        <p>Vui lòng vào website admin đẻ xem thông tin chi tiết</p>
+        `,
+      });
+
       res.status(201).json(data);
     } catch (error) {
+      console.log(error);
       throw error;
     }
   },
